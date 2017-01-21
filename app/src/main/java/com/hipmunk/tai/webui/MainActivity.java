6abd506f19482414fe6770c9a3f8043c68db0e17
Tai Vu <vutaia@gmail.com>
2017-01-20 17:11:33 -0800

    Initial commit

---
+++ app/src/main/java/com/hipmunk/tai/webui/MainActivity.java
@@ -0,0 +1,20 @@
+package com.hipmunk.tai.webui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView mWebView = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mWebView = (WebView) findViewById(R.id.webview);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.loadUrl("www.google.com");
+    }
+
+}
